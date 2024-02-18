2e32af374c57086dec17aace66740ba8b2d68f36
Tejas Amol Hande <59686002+tejhande@users.noreply.github.com>
2024-02-18 15:54:08 +0530

    Add files via upload

---
+++ Q1reverseString.py
@@ -0,0 +1,15 @@
+# Twitter:-- @DSA_Python
+# Reverse a string
+def reverse_string(s):
+    return s[::-1]
+
+# Test the function
+test_string = "hello"
+reversed_string = reverse_string(test_string)
+print(reversed_string)
+
+# What is the output of the following code?
+# a) 'olleh'
+# b) 'hello'
+# c) 'olle'
+# d) 'hell'
