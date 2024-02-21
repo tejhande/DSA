7e4625c7df62ea6b05b758bc72aa07ef34518f3e
Tejas Amol Hande <59686002+tejhande@users.noreply.github.com>
2024-02-21 22:06:58 +0530

    Add files via upload

---
+++ Q4palindrome.py
@@ -0,0 +1,16 @@
+# Q3 answer =  d) [4, 5, 1, 2, 3]
+# Twitter:-- @DSA_Python
+# Check if a string is a palindrome
+def is_palindrome(s):
+    return s == s[::-1]
+
+# Test the function
+test_string = "racecar"
+result = is_palindrome(test_string)
+print(result)
+
+# What is the output of the following code?
+# a) False
+# b) racecar
+# c) True
+# d) aceracr
