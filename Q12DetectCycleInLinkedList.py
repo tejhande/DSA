5a6ba0621b936ebee9ea086cf66c626e39d9d0c0
Tejas Amol Hande <59686002+tejhande@users.noreply.github.com>
2024-02-29 21:00:18 +0530

    Add files via upload

---
+++ Q12DetectCycleInLinkedList.py
@@ -0,0 +1,33 @@
+# Q11 answer = a) 1 -> 2 -> 3 -> None followed by 3 -> 2 -> 1 -> None
+# Twitter:-- @DSA_Python
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# Detect a cycle in a linked list
+def has_cycle(head):
+    slow = fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            return True
+    return False
+
+# Test the function
+node1 = ListNode(1)
+node2 = ListNode(2)
+node3 = ListNode(3)
+node1.next = node2
+node2.next = node3
+node3.next = node1  # Creating a cycle
+
+result = has_cycle(node1)
+print(result)
+
+# What is the output of the following code?
+# a) None
+# b) False
+# c) True
+# d) 1
